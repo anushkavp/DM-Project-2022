b4220459fdbb6ba20086bb135fb50b330193db11
Anushka Vuppala <anushka.vp27@gmail.com>
2022-12-18 19:11:40 -0500

    logistic regression and SVM in final py file

--- Final_bank_marketing_data.py
+++ Final_bank_marketing_data.py
@@ -514,16 +514,241 @@ print(new_df.head())
 
 #%%
 # convert boolean into numerical
-new_df.default = new_df.default.map(dict(yes=1, no=0))
-new_df.housing = new_df.housing.map(dict(yes=1, no=0))
-new_df.loan = new_df.loan.map(dict(yes=1, no=0))
-new_df.is_subscribed = new_df.is_subscribed.map(dict(yes=1, no=0))
+new_df['default'] = df['default'].map({'no':0,'yes': 1})
+new_df['housing'] = df['housing'].map({'no':0,'yes':1})
+new_df['loan'] = df['loan'].map({'no':0,'yes':1})
+new_df['is_subscribed'] = df['is_subscribed'].map({'no':0,'yes':1})
 
+new_df["default"] = new_df["default"].astype(float)
+new_df["housing"] = new_df["housing"].astype(float)
+new_df["loan"] = new_df["loan"].astype(float)
+new_df["is_subscribed"] = new_df["is_subscribed"].astype(float)
 new_df.head()
 # %%
 X, y = new_df.iloc[:, :-1], new_df.iloc[:, -1]
 # Train/test set generation
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.3, random_state=123
+    X, y, test_size=0.2, random_state=123
 )
+
+
+#%%
+# Base model
+from sklearn.linear_model import LogisticRegression 
+logisticRegr1 = LogisticRegression()
+_ = logisticRegr1.fit(X_train, y_train)
+logisticRegr1.score(X_test, y_test)
+from sklearn.metrics import classification_report
+y_true, y_pred = y_test, logisticRegr1.predict(X_test)
+print(classification_report(y_true, y_pred))
+
+#%% RFE
+from sklearn.feature_selection import RFE
+
+# Init the transformer
+rfe = RFE(estimator= LogisticRegression(), n_features_to_select=10)
+
+# Fit to the training data
+_ = rfe.fit(X_train, y_train)
+# took 50 seconds
+
+print("The best 10 columns are =>",X_train.loc[:, rfe.support_].columns)
+
+#%% 
+# model with few dimensions
+logisticRegr2 = LogisticRegression()
+_ = logisticRegr2.fit(X_train.loc[:, rfe.support_], y_train)
+logisticRegr2.score(X_test.loc[:,['age', 'default', 'balance', 'housing', 'loan', 'contact', 'duration',
+       'campaign', 'pdays', 'previous']], y_test)
+from sklearn.metrics import classification_report
+y_true, y_pred = y_test, logisticRegr2.predict(X_test.loc[:,['age', 'default', 'balance', 'housing', 'loan', 'contact', 'duration',
+       'campaign', 'pdays', 'previous']])
+print(classification_report(y_true, y_pred))
+
+#%% smote
+X_train = X_train.loc[:,['age', 'default', 'balance', 'housing', 'loan', 'contact', 'duration',
+       'campaign', 'pdays', 'previous']]
+X_test = X_test.loc[:,['age', 'default', 'balance', 'housing', 'loan', 'contact', 'duration',
+       'campaign', 'pdays', 'previous']]
+
+from sklearn.linear_model import LogisticRegression
+from imblearn.over_sampling import SMOTE
+os = SMOTE(random_state=123)
+columns = X_train.columns
+
+os_data_X,os_data_y=os.fit_resample(X_train, y_train)
+os_data_X = pd.DataFrame(data=os_data_X,columns=columns )
+os_data_y= pd.DataFrame(data=os_data_y)
+
+logisticRegr = LogisticRegression()
+logisticRegr.fit(os_data_X, os_data_y)
+
+print('Logit model accuracy (with the test set):', logisticRegr.score(X_test, y_test))
+print('Logit model accuracy (with the train set):', logisticRegr.score(os_data_X, os_data_y))
+
+print(logisticRegr.predict(X_test))
+
+test = logisticRegr.predict_proba(X_test)
+type(test)
+
+from sklearn.metrics import classification_report
+y_true, y_pred = y_test, logisticRegr.predict(X_test)
+print(classification_report(y_true, y_pred))
+
+
+
+#%% 
+# ROC-AUC score
+from sklearn.metrics import roc_auc_score, roc_curve
+
+# generate a no skill prediction (majority class)
+ns_probs = [0 for _ in range(len(y_test))]
+# predict probabilities
+lr_probs = logisticRegr.predict_proba(X_test)
+# keep probabilities for the positive outcome only
+lr_probs = lr_probs[:, 1]
+# calculate scores
+ns_auc = roc_auc_score(y_test, ns_probs)
+lr_auc = roc_auc_score(y_test, lr_probs)
+# summarize scores
+print('No Skill: ROC AUC=%.3f' % (ns_auc))
+print('Logistic: ROC AUC=%.3f' % (lr_auc))
+# calculate roc curves
+ns_fpr, ns_tpr, _ = roc_curve(y_test, ns_probs)
+lr_fpr, lr_tpr, _ = roc_curve(y_test, lr_probs)
+# plot the roc curve for the model
+plt.plot(ns_fpr, ns_tpr, linestyle='--', label='No Skill')
+plt.plot(lr_fpr, lr_tpr, marker='.', label='Logistic')
+# axis labels
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+# show the legend
+plt.legend()
+# show the plot
+plt.show()
 # %%
+# Statsmodel
+
+import statsmodels.api as sm 
+from statsmodels.formula.api import glm
+from sklearn.model_selection import train_test_split
+
+X, y = new_df.iloc[:, :-1], new_df.iloc[:, -1]
+# Train/test set generation
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=123
+)
+
+from sklearn.linear_model import LogisticRegression
+from imblearn.over_sampling import SMOTE
+os = SMOTE(random_state=123)
+columns = X_train.columns
+
+os_data_X,os_data_y=os.fit_resample(X_train, y_train)
+os_data_X = pd.DataFrame(data=os_data_X,columns=columns )
+os_data_y= pd.DataFrame(data=os_data_y)
+
+os_data = pd.concat([os_data_X.reset_index(drop=True), os_data_y], axis=1)
+os_test = pd.concat([X_test.reset_index(drop=True), y_test], axis=1)
+
+modelLogit = glm(formula='is_subscribed ~ C(marital)+C(education)+C(default)+C(housing)+C(loan)+C(contact)+C(month)+duration+campaign+C(poutcome)', data=os_data, family=sm.families.Binomial())
+
+modelLogitFit = modelLogit.fit()
+print( modelLogitFit.summary() )
+
+modelpredicitons = pd.DataFrame( columns=['Predicted'], data= modelLogitFit.predict(pd.concat([X_test, y_test], axis=1))) 
+cut_off = 0.3
+# Compute class predictions
+modelpredicitons['Predictions'] = np.where(modelpredicitons['Predicted'] > cut_off, 1, 0)
+print(modelpredicitons.Predictions.head())
+#
+# Make a cross table
+confusionmatrix = (pd.crosstab(os_test.y, modelpredicitons.Predictions,
+rownames=['Actual'], colnames=['Predicted'],
+margins = True))
+
+print(pd.crosstab(os_test.y, modelpredicitons.Predictions,
+rownames=['Actual'], colnames=['Predicted'],
+margins = True))
+
+TN = confusionmatrix.iloc[0,0]
+FP = confusionmatrix.iloc[0,1]
+FN = confusionmatrix.iloc[1,0]
+TP = confusionmatrix.iloc[1,1]
+accuracy = (TP+TN)/(TP+TN+FP+FN)
+precision = TP / (TP + FP)
+recall = TP / (TP + FN)
+specificity = TN / (TN + FP)
+print("Accuracy = ", accuracy)
+print("Precision = ",precision)
+print("Recall/Sensitivity = ",recall)
+print("Specificity = ",specificity)
+print("F1 score = ",(2*(precision)*(recall))/(precision + recall))
+print("AUC score = ",roc_auc_score(y_test, modelLogitFit.predict(pd.concat([X_test, y_test], axis=1))))
+
+# %%
+# Cross fold validations
+from sklearn.model_selection import cross_val_score
+
+lr_cv_acc = cross_val_score(logisticRegr, os_data_X, os_data_y, cv= 5, scoring='accuracy' )
+print(f'LR CV accuracy score:  {lr_cv_acc}')
+
+
+# SVC
+# (took 8 mins to execute)
+from sklearn.metrics import confusion_matrix 
+from sklearn.metrics import classification_report
+from sklearn.svm import SVC, LinearSVC
+clf1 = LinearSVC()
+clf2 = SVC(kernel="linear")
+clf3 = SVC()
+clf4 = SVC(kernel='rbf', probability=True, C=1, gamma=0.1)
+classifiers = [clf1,clf2,clf3, clf4] 
+
+# for c in classifiers:
+#     print("Classifier: ",c)
+#     c.fit(os_data_X,os_data_y) 
+#     print(f'svc train score:  {c.score(X_train,y_train)}')
+#     print(f'svc test score:  {c.score(X_test,y_test)}')
+#     pred = c.predict(X_test)
+#     print(confusion_matrix(y_test, pred))
+#     print(classification_report(y_test, pred))
+clf1.fit(os_data_X, os_data_y)
+print(f'svc train score:  {clf1.score(X_train,y_train)}')
+print(f'svc test score:  {clf1.score(X_test,y_test)}')
+pred = clf1.predict(X_test)
+print(confusion_matrix(y_test, clf1.predict(X_test)))
+print(classification_report(y_test, clf1.predict(X_test)))
+
+
+#%%
+from sklearn.metrics import roc_curve
+from sklearn.metrics import auc
+
+# Compute fpr, tpr, thresholds and roc auc
+fpr, tpr, thresholds = roc_curve(y_test, pred)
+roc_auc = auc(fpr, tpr)
+
+# Plot ROC curve
+plt.plot(fpr, tpr, label='ROC curve (area = %0.3f)' % roc_auc)
+plt.plot([0, 1], [0, 1], 'k--')  # random predictions curve
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.0])
+plt.xlabel('False Positive Rate or (1 - Specifity)')
+plt.ylabel('True Positive Rate or (Sensitivity)')
+plt.title('Receiver Operating Characteristic')
+plt.legend(loc="lower right")
+# %%
+
+# SVC with different gamma values
+# took 17 mins to execute
+gammas = ["auto","scale", 0.1,1]
+for gamma in gammas:
+    print("GAMMA = ", gamma)
+    svc = SVC(gamma=gamma).fit(os_data_X, os_data_y)
+    print(f'svc train score:  {svc.score(os_data_X, os_data_y)}')
+    print(f'svc test score:  {svc.score(X_test,y_test)}')
+    pred = svc.predict(X_test)
+    print(confusion_matrix(y_test, pred))
+    print(classification_report(y_test, pred))
+    
